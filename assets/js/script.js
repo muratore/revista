7e56be092c6dd95016bcdc0f0fb473452d136a94
Alessandro Muratore <muratore@gmail.com>
2022-11-24 13:06:17 -0300

    Edição 64 enviada

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+// const body = document.querySelector('body');
+// const footer = document.querySelector("footer");
+
+
+// const heightWindow = (window.innerHeight - 40).toString();
+
+// footer.style.position = 'absolute';
+// footer.style.top = `${heightWindow}px`
+
+
+// const output = document.querySelector("#output");
+
+// window.addEventListener('resize', (e)=>{
+//     let scrollHeightResize = (Math.max(
+//         document.body.scrollHeight, document.documentElement.scrollHeight,
+//         document.body.offsetHeight, document.documentElement.offsetHeight,
+//         document.body.clientHeight, document.documentElement.clientHeight
+//       ) - 40).toString();
+//       footer.style.top = `${scrollHeightResize}px`
+      
+// })
+// window.addEventListener('scroll', (e)=>{
+//     let scrollHeightScroll = (Math.max(
+//         document.body.scrollHeight, document.documentElement.scrollHeight,
+//         document.body.offsetHeight, document.documentElement.offsetHeight,
+//         document.body.clientHeight, document.documentElement.clientHeight
+//       )- 40).toString();
+//       footer.style.top = `${scrollHeightScroll}px`
+      
+// })
+// window.addEventListener("scroll", (event) => {
+//     output.textContent = `scrollTop: ${window.scrollY}`;
+//   });
+
+
+
+
+// const ySize = (window.screen.height - 240).toString();
+// console.log(ySize);
+// console.log(footer);
+// footer.style.position = 'fixed';
+// footer.style.top = `${ySize}px`;
+
+// window.addEventListener("scroll", (event) => {
+//     output.textContent = `scrollTop: ${window.scrollY}`;
+//   });
